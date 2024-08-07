74af11ec1709602ebae4b00a1ed2eb844cf853ed
kseunghui@gmail.com <kseunghui@gmail.com>
2024-08-07 11:46:19 +0900

    [feat] UICollectionView 및 CollectionViewCell 구현

--- PokeDex/View/MainViewController.swift
+++ PokeDex/View/MainViewController.swift
@@ -10,12 +10,96 @@ import RxSwift
 import SnapKit
 
 class MainViewController: UIViewController {
+    
+    private let viewModel = MainViewModel()
+    private let disposeBag = DisposeBag()
+    private var pokemonDetail = [PokemonDetail]()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "pokemonBall")
+        return imageView
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.register(PokedexCell.self, forCellWithReuseIdentifier: PokedexCell.id)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .darkRed
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configUI()
+        bind()
+    }
+    
+    func bind() {
+        viewModel.pokemonSubject
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] detail in
+                print("bind: \(detail)")
+                self?.pokemonDetail = detail
+                self?.collectionView.reloadData()
+            }, onError: { error in
+                print("에러 발생: \(error)")
+            }).disposed(by: disposeBag)
     }
 
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item, item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.interGroupSpacing = 10
+        
+//        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(44))
+//        let header = NSCollectionLayoutBoundarySupplementaryItem (layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+//        section.boundarySupplementaryItems = [header]
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func configUI() {
+        view.backgroundColor = .mainRed
+        [imageView, collectionView].forEach { view.addSubview($0) }
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(50)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(40)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide.snp.horizontalEdges)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+}
 
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("count: \(pokemonDetail.count)")
+        return pokemonDetail.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PokedexCell.id, for: indexPath) as? PokedexCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(with: pokemonDetail[indexPath.row])
+        return cell
+    }
 }
 
--- PokeDex/View/PokedexCell.swift
+++ PokeDex/View/PokedexCell.swift
@@ -8,5 +8,39 @@
 import UIKit
 
 class PokedexCell: UICollectionViewCell {
+    static let id = "PokedexCell"
     
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .cellBackground
+        contentView.addSubview(imageView)
+        imageView.frame = contentView.bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with pokemonDetail: PokemonDetail) {
+        guard let id = pokemonDetail.id else { return }
+        let urlString = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(id).png"
+        guard let url = URL(string: urlString) else { return }
+        
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.imageView.image = image
+                    }
+                }
+            }}
+    }
 }
