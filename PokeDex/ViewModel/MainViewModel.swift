420d6a5d62819547bfa18c1431975246d27489a5
kseunghui@gmail.com <kseunghui@gmail.com>
2024-08-07 11:45:50 +0900

    [fix] 구조에 맞게 ViewModel 수정

--- PokeDex/ViewModel/MainViewModel.swift
+++ PokeDex/ViewModel/MainViewModel.swift
@@ -15,7 +15,7 @@ import RxSwift
 class MainViewModel {
     private let disposeBag = DisposeBag()
     
-    let pokemonSubject = BehaviorSubject(value: [Pokemon]())
+    let pokemonSubject = BehaviorSubject(value: [PokemonDetail]())
     init() {
         fetchPokemon()
     }
@@ -28,10 +28,30 @@ class MainViewModel {
         
         NetworkManager.shared.fetch(url: url)
             .subscribe(onSuccess: { [weak self] (pokemonResponse: PokemonResponse) in
-                self?.pokemonSubject.onNext(pokemonResponse.results)
+                let pokemonDetails = pokemonResponse.results.compactMap { self?.fetchPokemonDetail(from: $0.url) }
+                
+                Observable.zip(pokemonDetails)
+                    .observe(on: MainScheduler.instance)
+                    .subscribe(onNext: { [weak self] details in
+                        print("1번: \(details)")
+                        self?.pokemonSubject.onNext(details)},
+                    onError: { error in
+                        print("2번: 에러")
+                        self?.pokemonSubject.onError(error)
+                    }).disposed(by: self?.disposeBag ?? DisposeBag())
+                
             }, onFailure: { [weak self] error in
                 self?.pokemonSubject.onError(error)
             }).disposed(by: disposeBag)
+    }
+    
+    func fetchPokemonDetail(from urlString: String?) -> Observable<PokemonDetail> {
+        guard let urlString = urlString,
+              let pokemonID = urlString.split(separator: "/").last,
+              let url = URL(string: "https://pokeapi.co/api/v2/pokemon/\(pokemonID)/") else {
+            return Observable.error(NetworkError.invaildUrl)
+        }
+        return NetworkManager.shared.fetch(url: url).asObservable()
     }
 }
 
