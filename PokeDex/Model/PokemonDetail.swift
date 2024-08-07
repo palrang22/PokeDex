38602f8a8f787cb5a2ba7d23cb38769a3e18164d
kseunghui@gmail.com <kseunghui@gmail.com>
2024-08-07 11:44:58 +0900

    [feat] 구조체 수정

--- PokeDex/Model/PokemonDetail.swift
+++ PokeDex/Model/PokemonDetail.swift
@@ -10,7 +10,15 @@ import Foundation
 struct PokemonDetail: Codable {
     let id: Int?
     let name: String?
-    let types: String?
+    let types: [Types]?
     let height: Int?
     let weight: Int?
+}
+
+struct Types: Codable {
+    let type: TypeInfo
+}
+
+struct TypeInfo: Codable {
+    let name: String
 }
