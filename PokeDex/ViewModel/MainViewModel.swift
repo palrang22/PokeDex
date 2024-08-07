47c1c53bf976cc6257d6c3dee4e8a23dde6b8c8c
kseunghui@gmail.com <kseunghui@gmail.com>
2024-08-07 09:18:38 +0900

    [feat] fetch Pokemon data

--- PokeDex/ViewModel/MainViewModel.swift
+++ PokeDex/ViewModel/MainViewModel.swift
@@ -7,3 +7,29 @@
 
 import UIKit
 import RxSwift
+
+
+//
+// https://pokeapi.co/api/v2/pokemon/\(pokemon_id)/
+
+class MainViewModel {
+    private let disposeBag = DisposeBag()
+    
+    let pokemonSubject = BehaviorSubject(value: [Pokemon]())
+    init() {}
+    
+    func fetchPokemon() {
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0") else {
+            pokemonSubject.onError(NetworkError.invaildUrl)
+            return
+        }
+        
+        NetworkManager.shared.fetch(url: url)
+            .subscribe(onSuccess: { [weak self] (pokemonResponse: PokemonResponse) in
+                self?.pokemonSubject.onNext(pokemonResponse.results)
+            }, onFailure: { [weak self] error in
+                self?.pokemonSubject.onError(error)
+            }).disposed(by: disposeBag)
+    }
+}
+
